2cb0d14c8995fccac5407eb410f9f3ed54f4d56d
Fayjul Islam Al Imran <fayjulislamalimran7@gmail.com>
2022-10-01 10:02:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import GYMBody from './components/GYMBody/GYMBody';
-import Header from './components/Header/Header';
-import QA from './components/QA/QA';
+import "./App.css";
+import GYMBody from "./components/GYMBody/GYMBody";
+import Header from "./components/Header/Header";
+import QA from "./components/QA/QA";
 
 function App() {
   return (
     <div className="App">
-    <Header></Header>
-    <GYMBody></GYMBody>
-    <QA></QA>
+      <Header></Header>
+      <GYMBody></GYMBody>
+      <QA></QA>
     </div>
   );
 }
